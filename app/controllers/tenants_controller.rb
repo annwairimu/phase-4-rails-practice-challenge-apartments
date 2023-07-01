55566a69ae9807c7c0491d6f3f85b5e6ca941018
Ann Wangui Wairimu <ann99wangui@gmail.com>
2023-07-01 13:38:21 +0300

    done

---
+++ app/controllers/tenants_controller.rb
@@ -0,0 +1,45 @@
+class TenantsController < ApplicationController
+    before_action :set_tenant, only: [:show, :update, :destroy]
+  
+    def index
+      @tenants = Tenant.all
+      render json: @tenants
+    end
+  
+    def show
+      render json: @tenant
+    end
+  
+    def create
+      @tenant = Tenant.new(tenant_params)
+  
+      if @tenant.save
+        render json: @tenant, status: :created
+      else
+        render json: @tenant.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      if @tenant.update(tenant_params)
+        render json: @tenant
+      else
+        render json: @tenant.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @tenant.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def set_tenant
+      @tenant = Tenant.find(params[:id])
+    end
+  
+    def tenant_params
+      params.require(:tenant).permit(:name, :age)
+    end
+  end
